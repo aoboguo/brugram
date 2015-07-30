f3d3c47b41de43157a5e50f2fc85957a46e3948a
Aobo Guo <aobo.guo@gmail.com>
2015-07-30 16:34:03 +0200

    Added photo index and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Photo.delete_all
 User.delete_all
 
-User.create! email: "t@t.com", password: "password", password_confirmation: "password"
+user = User.create! email: "cameron.averill@twiceyalerejects.com", password: "password", password_confirmation: "password"
+
+Photo.create! caption: "transformation tuesday", image: "/img/cam-selfie.jpg", :public => true, user: user
+Photo.create! caption: "taken when I was still training at the Parisian Opera Ballet", image: "/img/ballet.jpg", :public => true, user: user
